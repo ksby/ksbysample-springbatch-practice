f07c7c8e7886cded0b125ed9f2b6d4ac22d09eda
ksby <kusa@db3.so-net.ne.jp>
2021-02-20 16:41:50 +0000

    tasklet を bean に分離し、出力する文字列を name JobParameter で受け取るようにしました

--- hello-world-batch/src/main/java/ksbysample/batch/helloworld/HelloWorldBatchApplication.java
+++ hello-world-batch/src/main/java/ksbysample/batch/helloworld/HelloWorldBatchApplication.java
@@ -2,11 +2,9 @@ package ksbysample.batch.helloworld;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.boot.SpringApplication;
@@ -14,7 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 // 実行用コマンド
-// java -jar hello-world-batch/build/libs/hello-world-batch-0.0.1-SNAPSHOT.jar
+// java -jar hello-world-batch/build/libs/hello-world-batch-0.0.1-SNAPSHOT.jar name="tanaka taro"
 @EnableBatchProcessing
 @SpringBootApplication
 public class HelloWorldBatchApplication {
@@ -30,20 +28,28 @@ public class HelloWorldBatchApplication {
     }
 
     @Bean
-    public Step step() {
-        return this.stepBuilderFactory.get("step1")
-                .tasklet((contribution, chunkContext) -> {
-                    System.out.println("★★★ Hello, World!");
-                    return RepeatStatus.FINISHED;
-                })
+    public Job job() {
+        return this.jobBuilderFactory.get("basicJob")
+                .start(step1())
                 .build();
     }
 
     @Bean
-    public Job job() {
-        return this.jobBuilderFactory.get("job")
-                .start(step())
+    public Step step1() {
+        return this.stepBuilderFactory.get("step1")
+                .tasklet(helloWorldTasklet())
                 .build();
+    }
+
+    @Bean
+    public Tasklet helloWorldTasklet() {
+        return (contribution, chunkContext) -> {
+            String name = (String) chunkContext.getStepContext()
+                    .getJobParameters()
+                    .get("name");
+            System.out.println(String.format("★★★ Hello, %s", name));
+            return RepeatStatus.FINISHED;
+        };
     }
 
     public static void main(String[] args) {
