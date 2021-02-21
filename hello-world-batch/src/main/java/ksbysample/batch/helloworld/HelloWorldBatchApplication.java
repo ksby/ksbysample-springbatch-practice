6ca34529e816972480bf340ecfac9744b2c16645
ksby <kusa@db3.so-net.ne.jp>
2021-02-21 04:35:05 +0000

    run.id を自動でインクリメントする処理を追加しました（これで同じパラメータで実行してもエラーになりません）

--- hello-world-batch/src/main/java/ksbysample/batch/helloworld/HelloWorldBatchApplication.java
+++ hello-world-batch/src/main/java/ksbysample/batch/helloworld/HelloWorldBatchApplication.java
@@ -9,6 +9,7 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.CompositeJobParametersValidator;
 import org.springframework.batch.core.job.DefaultJobParametersValidator;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Value;
@@ -39,6 +40,7 @@ public class HelloWorldBatchApplication {
         return this.jobBuilderFactory.get("basicJob")
                 .start(step1())
                 .validator(validator())
+                .incrementer(new RunIdIncrementer())
                 .build();
     }
 
@@ -68,7 +70,7 @@ public class HelloWorldBatchApplication {
         DefaultJobParametersValidator defaultJobParametersValidator =
                 new DefaultJobParametersValidator(
                         new String[]{"fileName"},
-                        new String[]{"name"}                );
+                        new String[]{"name", "run.id"}                );
         defaultJobParametersValidator.afterPropertiesSet();
 
         validator.setValidators(Arrays.asList(new Parametervalidator(),
